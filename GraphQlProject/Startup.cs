997505b06bc6a1bf3c6471c6401f693bc686aa06
Ryan Waszak <rwaszak@tesla.com>
2021-04-23 10:07:43 -0700

    add .gitignore and update services in startup

--- GraphQlProject/Startup.cs
+++ GraphQlProject/Startup.cs
@@ -1,26 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using GraphiQl;
 using GraphQL.Server;
 using GraphQL.Types;
 using GraphQlProject.Data;
 using GraphQlProject.Interfaces;
-using GraphQlProject.Mutation;
 using GraphQlProject.Query;
 using GraphQlProject.Schema;
 using GraphQlProject.Services;
 using GraphQlProject.Type;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GraphQlProject
 {
@@ -40,10 +33,15 @@ namespace GraphQlProject
             services.AddTransient<IMenu, MenuService>();
             services.AddTransient<ISubMenu, SubMenuService>();
             services.AddTransient<IReservation, ReservationService>();
-            services.AddTransient<ProductType>();
-            services.AddTransient<ProductQuery>();
-            services.AddTransient<ProductMutation>();
-            services.AddTransient<ISchema, ProductSchema>();
+            services.AddTransient<MenuType>();
+            services.AddTransient<ReservationType>();
+            services.AddTransient<SubMenuType>();
+            services.AddTransient<MenuQuery>();
+            services.AddTransient<SubMenuQuery>();
+            services.AddTransient<ReservationQuery>();
+            services.AddTransient<RootQuery>();
+            //services.AddTransient<ProductMutation>();
+            services.AddTransient<ISchema, RootSchema>();
 
             services.AddGraphQL(options => 
             {
